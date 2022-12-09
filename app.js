367dc42864373fad40750a56a85b4f39dbc0c151
SimonHung8 <simon850226@gmail.com>
2022-12-09 10:35:22 +0800

    feat: add body-parser, json-parser, method-override

--- app.js
+++ app.js
@@ -3,17 +3,17 @@ if (process.env.NODE_ENV !== 'production') {
 }
 
 const express = require('express')
+const methodOverride = require('method-override')
 const passport = require('./config/passport')
-// const helpers = require('./_helpers')
 
 const app = express()
 const port = 3000
 
-// // use helpers.getUser(req) to replace req.user
-
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
 app.use(passport.initialize())
+app.use(methodOverride('_method'))
 
-app.get('/', (req, res) => res.send('Hello World!'))
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
 module.exports = app
